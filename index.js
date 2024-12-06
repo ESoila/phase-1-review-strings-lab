d6838d43bec06a73d520c0d6bdd312c0ef69161b
soila <elizabeth.soila@student.moringaschool.com>
2024-12-06 15:42:41 +0300

    phase-1-review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+
+
+
+// Define currentUser with a string of your choice
+const currentUser = 'Grace Hopper';
+
+// Define a welcome message with string interpolation
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create an excited version of the welcome message by converting it to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define a short greeting that uses the first character of the currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
